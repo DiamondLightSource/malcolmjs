7d65f760809513175e9bd0cf228d4564e40089f8
Ian Gillingham <ian.gillingham@diamond.ac.uk>
2016-07-18 15:21:21 +0100

    tests.webpack.js: configured for Karma testing

--- tests.webpack.js
+++ tests.webpack.js
@@ -1,2 +1,3 @@
 var context = require.context('./js', true, /-test\.js$/); //make sure you have your directory and regex test set correctly!
+console.log("tests.webpack.js...");
 context.keys().forEach(context);
