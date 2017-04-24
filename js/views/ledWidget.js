cc85e1361c45100ccc315ebb6e6501795da748fd
Ian Gillingham <ian.gillingham@diamond.ac.uk>
2017-04-24 09:23:52 +0100

    ledWidget.js: Added px to values for SVG element width and height in style.

--- js/views/ledWidget.js
+++ js/views/ledWidget.js
@@ -21,7 +21,7 @@ let LEDWidget = React.createClass({
     {
     return (
 
-      <svg style={{width: '150', height: '20'}}>
+      <svg style={{width: '150px', height: '20px'}}>
         <circle r="8" className={"ledWidget" + (this.props.blockAttributeValue ? 'ON' : 'OFF')}
                 transform="translate(9, 11)"/>
       </svg>
