d5fd1ea3264941eb5c11338417b8d119769005c9
Ian Gillingham <ian.gillingham@diamond.ac.uk>
2017-05-15 11:33:06 +0100

    config.js: Transmuted Config from simple Object to ES6 Class.

--- js/utils/config.js
+++ js/utils/config.js
@@ -2,84 +2,128 @@
  * Created by Ian Gillingham on 16/09/16.
  */
 
-let config = {
-
-  setProtocolVersion: function(protocolVersion)
-    {
-    /* Set the Zebra or PandA communication protocol version
-     */
-
-    if (protocolVersion in _protocolEnum)
-      {
-      _protocolKey = protocolVersion;
-      }
-    },
-
-  getProtocolVersion: function()
-  {
-    /* Get the Zebra or PandA communication protocol version
-     */
-    return _protocolKey;
-  },
-
-  getProtocolVersionList: function()
-    {
-    let keys = Object.keys(_protocolEnum);
-    return(keys);
-    },
-
-  setServerName: function(name)
-    {
-      if (name in _serverURLTable)
-        {
-        _serverKey = name;
-        }
-    },
-
-  getServerNameList: function()
-    {
-    let keys = Object.keys(_serverURLTable);
-    return(keys);
-    },
-
-  getServerName: function()
-    {
-    return(_serverKey);
-    },
-
-  getServerURL: function()
-    {
-    let url = _serverURLTable[_serverKey];
-    return(url);
-    },
-
-  getDeviceName: function()
-  {
-    return(_deviceName);
-  },
-
-  setdeviceName: function(name)
-  {
-      _deviceName = name;
-  },
-
-  testparams: {maxBlocks: 10}
-
+/**
+ * @module exports a singleton instance of Config class
+ *
+ */
+const __port_colour = {
+  'bool'   : '#4921DB',
+  'int32'  : '#DB8C1D',
+  'default': '#000000'
 };
 
-let _protocolEnum = Object.freeze({V1_0: 0, V2_0: 1});
+/**
+ * @module exports a singleton instance of Config class
+ *
+ * @class Config
+ * @description  Provides essential run-time configuration information for MalcolmJS
+ *
+ */
+class Config {
+constructor()
+  {
+  this._protocolEnum = Object.freeze({V1_0: 0, V2_0: 1});
 
-/* Set default protocol */
-let _protocolKey = _protocolEnum.V2_0;
+  /* Set default protocol */
+  this._protocolKey = this._protocolEnum.V2_0;
 
-let _serverURLTable = Object.freeze({ pc70:'ws://pc0070.cs.diamond.ac.uk:8080/ws',
-                                      isaDev:'ws://172.23.252.202/ws',
-                                      isaSpare:'ws://172.23.252.201/ws',
-                                      simulator:'ws://localhost:8080/ws'});
+  this._serverURLTable = Object.freeze({
+    pc70     : 'ws://pc0070.cs.diamond.ac.uk:8080/ws',
+    isaDev   : 'ws://172.23.252.202/ws',
+    isaSpare : 'ws://172.23.252.201/ws',
+    simulator: 'ws://localhost:8080/ws'
+  });
 
-/* Default server URL to PC0070 - Ian's local machine */
-let _serverKey = 'simulator';
+  /* Default server URL to PC0070 - Ian's local machine */
+  this._serverKey = 'simulator';
 
-let _deviceName = 'P';
+  this._deviceName = 'P';
+
+  this.testparams = {maxBlocks: 10};
+  }
+
+setProtocolVersion(protocolVersion)
+  {
+  /* Set the Zebra or PandA communication protocol version
+   */
+
+  if (protocolVersion in this._protocolEnum)
+    {
+    this._protocolKey = protocolVersion;
+    }
+  }
+
+getProtocolVersion()
+  {
+  /* Get the Zebra or PandA communication protocol version
+   */
+  return this._protocolKey;
+  }
+
+getProtocolVersionList()
+  {
+  let keys = Object.keys(this._protocolEnum);
+  return (keys);
+  }
+
+setServerName(name)
+  {
+  if (name in this._serverURLTable)
+    {
+    this._serverKey = name;
+    }
+  }
+
+getServerNameList()
+  {
+  let keys = Object.keys(this._serverURLTable);
+  return (keys);
+  }
+
+getServerName()
+  {
+  return (_serverKey);
+  }
+
+getServerURL()
+  {
+  let url = this._serverURLTable[this._serverKey];
+  return (url);
+  }
+
+getDeviceName()
+  {
+  return (this._deviceName);
+  }
+
+setdeviceName(name)
+  {
+  this._deviceName = name;
+  }
+
+/**
+ *
+ * @param {string} portType
+ * @desc can presently be either 'bool' or 'int32'. If an unknown type is passes then the colour will default to black.
+ *
+ */
+getPortColour(portType)
+  {
+  let returnColour = __port_colour.default;
+
+  if (typeof(portType) === "string")
+    {
+    if (__port_colour.hasOwnProperty(portType))
+      {
+      returnColour = __port_colour[portType];
+      }
+    }
+  return (returnColour);
+  }
+
+
+}
+
+let config = new Config();
 
 export default config;
