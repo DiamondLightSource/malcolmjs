519c6620c5ddfbcba79526ed8ae4e9b9325a9004
Tom Cobb <tom.cobb@diamond.ac.uk>
2017-10-20 14:57:04 +0100

    Copied files to prep for changeing build system to react-scripts

--- tests.webpack.js
+++
@@ -1,3 +0,0 @@
-var context = require.context('./js', true, /-test\.js$/); //make sure you have your directory and regex test set correctly!
-console.log("tests.webpack.js...");
-context.keys().forEach(context);
