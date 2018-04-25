933a6b0363d37a7b4ec396641ed785a82e21ab1f
Ray Millward <raymond.millward@tessella.com>
2018-04-25 12:09:03 +0100

    Added a global decorator for the material theme in storybook stories

--- src/drawerContainer/drawerHeader.stories.js
+++ src/drawerContainer/drawerHeader.stories.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import DrawerHeader from './drawerHeader.component';
@@ -8,9 +7,5 @@ storiesOf('Drawer Header', module).add(
   'default',
   withInfo(`
   The header for the drawers either side for the parent and child block details.
-  `)(() => (
-    <MuiThemeProvider>
-      <DrawerHeader />
-    </MuiThemeProvider>
-  ))
+  `)(() => <DrawerHeader />)
 );
