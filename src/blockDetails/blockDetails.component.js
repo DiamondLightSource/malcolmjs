b8fe3fafc1c626afaaa0c410fc43f476ff8f020c
Ray Millward <raymond.millward@tessella.com>
2018-04-30 21:38:39 +0100

    Added a loading indicator to the block details

--- src/blockDetails/blockDetails.component.js
+++ src/blockDetails/blockDetails.component.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import { CircularProgress } from 'material-ui/Progress';
+import Typography from 'material-ui/Typography';
 
-const containerStyle = {
-  color: 'white',
-  backgroundColor: 'blue',
+const styles = theme => ({
+  progressContainer: {
+    marginTop: 15,
+    color: theme.palette.primary.contrastText,
+  },
+});
+
+const blockLoading = (block, classes) => {
+  if (block && block.loading) {
+    return (
+      <div className={classes.progressContainer}>
+        <CircularProgress color="secondary" />
+        <Typography color="inherit">Loading...</Typography>
+      </div>
+    );
+  }
+
+  return null;
 };
 
-const BlockDetails = () => (
-  <div style={containerStyle}>Block Details Placeholder</div>
+const BlockDetails = props => (
+  <div>{blockLoading(props.block, props.classes)}</div>
 );
 
-export default BlockDetails;
+BlockDetails.propTypes = {
+  block: PropTypes.shape({
+    loading: PropTypes.bool,
+  }),
+  classes: PropTypes.shape({
+    progressContainer: PropTypes.string,
+  }).isRequired,
+};
+
+BlockDetails.defaultProps = {
+  block: undefined,
+};
+
+export default withStyles(styles, { withTheme: true })(BlockDetails);
--- src/mainMalcolmView/mainMalcolmView.container.js
+++ src/mainMalcolmView/mainMalcolmView.container.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import DrawerContainer from '../drawerContainer/drawerContainer.component';
+import BlockDetails from '../blockDetails/blockDetails.component';
 
-const MainMalcolmView = () => (
+const MainMalcolmView = props => (
   <div>
     <DrawerContainer>
-      <div>Left content</div>
+      <BlockDetails block={props.parentBlock} />
       <div>Middle content</div>
       <div>Right content</div>
     </DrawerContainer>
   </div>
 );
 
-export default MainMalcolmView;
+const mapStateToProps = state => ({
+  parentBlock: state.malcolm.parentBlock
+    ? state.malcolm.blocks[state.malcolm.parentBlock]
+    : undefined,
+});
+
+const mapDispatchToProps = () => ({});
+
+MainMalcolmView.propTypes = {
+  parentBlock: PropTypes.shape({}),
+};
+
+MainMalcolmView.defaultProps = {
+  parentBlock: undefined,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainMalcolmView);
