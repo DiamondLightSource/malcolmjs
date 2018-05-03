f8c4c4a63114dc9815afd16dcd2b9923914de65d
Bryan Robert Tester <bryan.tester@diamond.ac.uk>
2018-05-03 09:50:38 +0100

    Fixed tests & Added test snapshots to repo

--- src/WidgetCheckbox/WidgetCheckbox.test.js
+++ src/WidgetCheckbox/WidgetCheckbox.test.js
@@ -1,29 +1,69 @@
 import React from 'react';
-import { createShallow } from 'material-ui/test-utils';
-import { action } from '@storybook/addon-actions/src/index';
+import { createShallow, createMount } from 'material-ui/test-utils';
 import WidgetCheckbox from './WidgetCheckbox.component';
 
 describe('WidgetCheckbox', () => {
   let shallow;
+  let mount;
 
   beforeEach(() => {
     shallow = createShallow();
+    mount = createMount();
   });
 
-  it('renders off', () => {
+  afterEach(() => {
+    mount.cleanUp();
+  });
+
+  it('renders unchecked', () => {
     const wrapper = shallow(
       <WidgetCheckbox
         CheckState={false}
-        checkEventHandler={action('checked')}
+        checkEventHandler={() => {}}
+        Label="TestBox"
       />
     );
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('renders on', () => {
+  it('calls check', () => {
+    const checkAction = jest.fn();
+    const wrapper = mount(
+      <WidgetCheckbox
+        CheckState={false}
+        checkEventHandler={() => {
+          checkAction();
+        }}
+        Label="TestBox"
+      />
+    );
+
+    wrapper.find('input').simulate('change');
+
+    expect(checkAction.mock.calls.length).toEqual(1);
+  });
+
+  it('renders checked', () => {
     const wrapper = shallow(
-      <WidgetCheckbox CheckState checkEventHandler={action('unchecked')} />
+      <WidgetCheckbox CheckState checkEventHandler={() => {}} Label="TestBox" />
     );
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('calls uncheck', () => {
+    const uncheckAction = jest.fn();
+    const wrapper = mount(
+      <WidgetCheckbox
+        CheckState
+        checkEventHandler={() => {
+          uncheckAction();
+        }}
+        Label="TestBox"
+      />
+    );
+
+    wrapper.find('input').simulate('change');
+
+    expect(uncheckAction.mock.calls.length).toEqual(1);
   });
 });
