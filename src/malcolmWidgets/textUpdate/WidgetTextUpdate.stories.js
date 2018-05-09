49ee2f2ae09875b76739d206aa85a3dbed977088
Bryan Robert Tester <bryan.tester@diamond.ac.uk>
2018-05-09 16:30:51 +0100

    Updated tests Added tests for TextUpdate widget and updated snapshots for others

--- src/malcolmWidgets/textUpdate/WidgetTextUpdate.stories.js
+++ src/malcolmWidgets/textUpdate/WidgetTextUpdate.stories.js
@@ -11,13 +11,13 @@ const TextUpdate = (text, units) => (
 
 storiesOf('WidgetTextUpdate', module)
   .add(
-    'Hello world',
+    'simple text',
     withInfo(`
   A simple field for displaying some text.
   `)(() => TextUpdate('Hello World!', null))
   )
   .add(
-    'Power level',
+    'text with units',
     withInfo(`
   A simple field for displaying some text (with units).
   `)(() => TextUpdate('1.21', 'GW'))
