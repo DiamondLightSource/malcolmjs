447ca96c797c45ba22273ad0ead4f251ab35d1a1
Bryan Robert Tester <bryan.tester@diamond.ac.uk>
2018-05-18 13:35:41 +0100

    Updated snackbar theming Properties now taken from theme (and can be easily changed)

--- src/Snackbar/snackbar.component.js
+++ src/Snackbar/snackbar.component.js
@@ -4,16 +4,31 @@ import Snackbar from 'material-ui/Snackbar';
 import IconButton from 'material-ui/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import { connect } from 'react-redux';
+import { withStyles } from 'material-ui/styles/index';
 import { malcolmSnackbarState } from '../malcolm/malcolmActionCreators';
+
+const styles = theme => ({
+  snackbarContent: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.textColor,
+    height: 20,
+    flexWrap: 'nowrap',
+  },
+  snackbar: {
+    margin: 2,
+  },
+});
 
 const MessageSnackBar = props => (
   <Snackbar
     open={props.open}
     message={props.message}
     onClose={props.handleClose}
-    autoHideDuration={4000}
+    autoHideDuration={5000}
+    className={props.classes.snackbar}
+    SnackbarContentProps={{ className: props.classes.snackbarContent }}
     action={[
-      <IconButton key="close" onClick={props.handleClose}>
+      <IconButton key="close" onClick={props.handleClose} color="secondary">
         <CloseIcon />
       </IconButton>,
     ]}
@@ -24,6 +39,10 @@ MessageSnackBar.propTypes = {
   open: PropTypes.bool.isRequired,
   message: PropTypes.string.isRequired,
   handleClose: PropTypes.func.isRequired,
+  classes: PropTypes.shape({
+    snackbar: PropTypes.string.isRequired,
+    snackbarContent: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -35,4 +54,6 @@ const mapDispatchToProps = dispatch => ({
   handleClose: () => dispatch(malcolmSnackbarState(false, '')),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(MessageSnackBar);
+export default connect(mapStateToProps, mapDispatchToProps)(
+  withStyles(styles, { withTheme: true })(MessageSnackBar)
+);
