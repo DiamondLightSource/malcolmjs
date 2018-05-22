da59e97ec618018d5d8bb5f9347dc3a5d13e1d2c
Ray Millward <raymond.millward@tessella.com>
2018-05-22 10:28:23 +0100

    Added interaction tests for the drawerContainer and the navbar

--- src/drawerContainer/drawerContainer.test.js
+++ src/drawerContainer/drawerContainer.test.js
@@ -1,30 +1,43 @@
 import React from 'react';
-import { createShallow } from 'material-ui/test-utils';
+import { createShallow, createMount } from 'material-ui/test-utils';
+import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import DrawerContainer from './drawerContainer.component';
+import {
+  openParentPanelType,
+  openChildPanelType,
+} from '../viewState/viewState.actions';
 
 const mockStore = configureStore();
-const dispatch = jest.fn();
 
 describe('DrawerContainer', () => {
   let shallow;
+  let mount;
+  let state;
 
   beforeEach(() => {
     shallow = createShallow({ dive: true });
-  });
+    mount = createMount();
 
-  it('renders correctly', () => {
-    const state = {
+    state = {
       viewState: {
         openParentPanel: true,
         openChildPanel: true,
       },
+      malcolm: {
+        navigation: ['PANDA'],
+      },
     };
+  });
 
+  afterEach(() => {
+    mount.cleanUp();
+  });
+
+  it('renders correctly', () => {
     const wrapper = shallow(
       <DrawerContainer
         store={mockStore(state)}
-        dispatch={dispatch}
         parentTitle="Parent"
         childTitle="Child"
       >
@@ -34,5 +47,57 @@ describe('DrawerContainer', () => {
       </DrawerContainer>
     );
     expect(wrapper.dive()).toMatchSnapshot();
+  });
+
+  it('closeParent calls the close method', () => {
+    const store = mockStore(state);
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <DrawerContainer parentTitle="Parent" childTitle="Child">
+          <div>Left</div>
+          <div>Middle</div>
+          <div>Right</div>
+        </DrawerContainer>
+      </Provider>
+    );
+
+    wrapper
+      .find('DrawerHeader')
+      .at(0)
+      .find('button')
+      .at(0)
+      .simulate('click');
+
+    const actions = store.getActions();
+    expect(actions.length).toEqual(1);
+    expect(actions[0].type).toBe(openParentPanelType);
+    expect(actions[0].openParentPanel).toBeFalsy();
+  });
+
+  it('closeChild calls the close method', () => {
+    const store = mockStore(state);
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <DrawerContainer parentTitle="Parent" childTitle="Child">
+          <div>Left</div>
+          <div>Middle</div>
+          <div>Right</div>
+        </DrawerContainer>
+      </Provider>
+    );
+
+    wrapper
+      .find('DrawerHeader')
+      .at(1)
+      .find('button')
+      .at(0)
+      .simulate('click');
+
+    const actions = store.getActions();
+    expect(actions.length).toEqual(1);
+    expect(actions[0].type).toBe(openChildPanelType);
+    expect(actions[0].openParentPanel).toBeFalsy();
   });
 });
