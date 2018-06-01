4f6011d2cfce7e115f37d90d321aaaa6e7063aaa
Bryan Robert Tester <bryan.tester@diamond.ac.uk>
2018-06-01 18:00:43 +0100

    Adjusted textUpdate unit display style to match that of textInput also adjusted so truncated text still displays units separately

--- src/malcolmWidgets/textUpdate/WidgetTextUpdate.component.js
+++ src/malcolmWidgets/textUpdate/WidgetTextUpdate.component.js
@@ -5,25 +5,63 @@ import Typography from '@material-ui/core/Typography';
 import { emphasize } from '@material-ui/core/styles/colorManipulator';
 
 const styles = theme => ({
-  textUpdate: {
+  div: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  textUpdate80: {
+    backgroundColor: emphasize(theme.palette.background.paper, 0.1),
+    padding: 4,
+    paddingRight: 9,
+    textAlign: 'Right',
+    width: '80%',
+  },
+  textUpdate100: {
     backgroundColor: emphasize(theme.palette.background.paper, 0.1),
     padding: 4,
     paddingRight: 16,
     textAlign: 'Right',
+    width: '100%',
+  },
+  unitBox: {
+    backgroundColor: emphasize(theme.palette.background.paper, 0.1),
+    color: emphasize(theme.palette.primary.contrastText, 0.2),
+    padding: 4,
+    paddingRight: 2,
+    width: '20%',
   },
 });
 
-const WidgetTextUpdate = props => (
-  <Typography className={props.classes.textUpdate} noWrap>
-    {props.Text} {props.Units ? props.Units : null}
-  </Typography>
-);
+const WidgetTextUpdate = props => {
+  if (!props.Units) {
+    return (
+      <div className={props.classes.div}>
+        <Typography className={props.classes.textUpdate100} noWrap>
+          {props.Text}
+        </Typography>
+      </div>
+    );
+  }
+  return (
+    <div className={props.classes.div}>
+      <Typography className={props.classes.textUpdate80} noWrap>
+        {props.Text}
+      </Typography>
+      <Typography className={props.classes.unitBox} noWrap>
+        {props.Units}
+      </Typography>
+    </div>
+  );
+};
 
 WidgetTextUpdate.propTypes = {
   Text: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   Units: PropTypes.string,
   classes: PropTypes.shape({
-    textUpdate: PropTypes.string,
+    textUpdate100: PropTypes.string,
+    textUpdate80: PropTypes.string,
+    unitBox: PropTypes.string,
+    div: PropTypes.string,
   }).isRequired,
 };
 
