edd046a1a3e9ead0d949c2f0823586970e05fa95
Tom Cobb <tom.cobb@diamond.ac.uk>
2018-06-15 13:31:09 +0100

    Simplified canned data generator. Corrected it to not ask for delta for .blocks

--- server/pyScripts/canned_data_generator.py
+++ server/pyScripts/canned_data_generator.py
@@ -1,98 +1,104 @@
 import json
 import shutil
+import os
+from collections import OrderedDict
+
 from tornado.websocket import websocket_connect
 from tornado import gen, options
 from tornado.ioloop import IOLoop
-from collections import OrderedDict
-from malcolm.core import Queue
-from commands import getstatusoutput
+
 from filestring_utils import write_lines_to_file, mkdir_p
 
 
 class MalcolmServerConnect:
     def __init__(self, socket):
-        self.msg_id = -1
         self.socket = socket
-        self.message = None
-        self.status = ""
-        self.responses = Queue()
+        self.request = None
+        self.response = None
         self._loop = IOLoop()
 
     @gen.coroutine
     def message_coroutine(self):
         conn = yield websocket_connect("ws://localhost:%s/ws" % self.socket)
-        msg = json.dumps(self.message)
+        msg = json.dumps(self.request)
         conn.write_message(msg)
         resp = yield conn.read_message()
-        resp = json.loads(resp, object_pairs_hook=OrderedDict)
-        self.responses.put(resp)
+        self.response = json.loads(resp, object_pairs_hook=OrderedDict)
         conn.close()
 
-    def send_subscribe(self, path):
-        self.msg_id += 1
-        msg = OrderedDict()
-        msg['typeid'] = "malcolm:core/Subscribe:1.0"
-        msg['id'] = self.msg_id
-        msg['path'] = path
-        msg['delta'] = True
-        self.message = msg
+    def send_subscribe(self, path, delta):
+        self.request = OrderedDict()
+        self.request['typeid'] = "malcolm:core/Subscribe:1.0"
+        self.request['id'] = "CANNED"
+        self.request['path'] = path
+        self.request['delta'] = delta
         self._loop.run_sync(self.message_coroutine)
+        if delta:
+            for change in self.response['changes']:
+                if len(change) == 2 and isinstance(change[1], dict) and \
+                        "timeStamp" in change[1]:
+                    for endpoint in ("secondsPastEpoch", "nanoseconds"):
+                        change[1]['timeStamp'][endpoint] = 0
+
+    def subscribe_request_response(self, path, delta=True):
+        self.send_subscribe(path, delta)
+        request_msg = json.dumps(self.request, indent=2)
+        response_msg = json.dumps(self.response, indent=2)
+        return request_msg, response_msg
 
 
 def main(get_all_blocks):
-
     try:
         SimServer = MalcolmServerConnect(8008)
     except Exception as e:
-        raise Exception('%s (Have you started the simulator + pymalcolm server?' % e)
+        raise Exception(
+                '%s (Have you started the simulator + pymalcolm server?' % e)
 
-    try:
-        shutil.rmtree('../canned_data')
-    except OSError as e:
-        if e == "[Errno 2] No such file or directory: '../canned_data'":
-            pass
+    canned_data = os.path.join(os.path.dirname(__file__), "..", "canned_data")
+    if os.path.exists(canned_data):
+        shutil.rmtree(canned_data)
 
     print "Getting blocks..."
-    block_path = "blocks"
-    mkdir_p('../canned_data/Sub/%s' % block_path)
-    SimServer.send_subscribe((".", "blocks"))
-    write_lines_to_file('../canned_data/Sub/%s/request_info.json' % block_path,
-                        json.dumps(SimServer.message, indent=2))
-    block_meta = SimServer.responses.get(timeout=2)
-    block_meta['id'] = "CANNED"
-    write_lines_to_file('../canned_data/Sub/%s/response_info.json' % block_path,
-                        json.dumps(block_meta, indent=2))
+    mkdir_p('%(canned_data)s/Sub/blocks' % locals())
+    request, response = SimServer.subscribe_request_response(
+            (".", "blocks"), delta=False)
+    write_lines_to_file(
+            '%(canned_data)s/Sub/blocks/request_info.json' % locals(),
+            request)
+    write_lines_to_file(
+            '%(canned_data)s/Sub/blocks/response_info.json' % locals(),
+            response)
 
     if get_all_blocks:
-        blocks_to_query = block_meta['value']
+        blocks_to_query = SimServer.response["value"]
     else:
-        blocks_to_query = ["PANDA", "PANDA:TTLIN1", "PANDA:INENC1", "PANDA:LUT1", "PANDA:SEQ1"]
+        blocks_to_query = ["PANDA", "PANDA:TTLIN1", "PANDA:INENC1",
+                           "PANDA:LUT1", "PANDA:SEQ1"]
 
     for block in blocks_to_query:
         print "Getting %s meta..." % block
         block_path = '/'.join(block.split(':'))
-        mkdir_p('../canned_data/Sub/%s' % block_path)
-        SimServer.send_subscribe((block, "meta"))
-        write_lines_to_file('../canned_data/Sub/%s/request_meta.json' % block_path,
-                            json.dumps(SimServer.message, indent=2))
-        block_meta = SimServer.responses.get(timeout=2)
-        block_meta['id'] = "CANNED"
-        write_lines_to_file('../canned_data/Sub/%s/response_meta.json' % block_path,
-                            json.dumps(block_meta, indent=2))
-        print "...Getting %s fields for %s..." % (len(block_meta["changes"][0][1]["fields"]), block)
-        for field in block_meta["changes"][0][1]["fields"]:
-            SimServer.send_subscribe((block, field))
-            write_lines_to_file('../canned_data/Sub/%s/request_%s.json' % (block_path, field),
-                                json.dumps(SimServer.message, indent=2))
-            block_field = SimServer.responses.get(timeout=2)
-            block_field['id'] = "CANNED"
-
-            if 'timeStamp' in block_field['changes'][0][1].keys():
-                block_field['changes'][0][1]['timeStamp']['secondsPastEpoch'] = 0
-                block_field['changes'][0][1]['timeStamp']['nanoseconds'] = 0
-
-            write_lines_to_file('../canned_data/Sub/%s/response_%s.json' % (block_path, field),
-                                json.dumps(block_field, indent=2))
+        mkdir_p('%(canned_data)s/Sub/%(block_path)s' % locals())
+        request, response = \
+                SimServer.subscribe_request_response((block, "meta"))
+        write_lines_to_file(
+                '%(canned_data)s/Sub/%(block_path)s/request_meta.json'
+                        % locals(), request)
+        write_lines_to_file(
+                '%(canned_data)s/Sub/%(block_path)s/response_meta.json'
+                        % locals(), response)
+        block_meta = SimServer.response["changes"][0][1]
+        print "...Getting %s fields for %s..." % (
+                len(block_meta["fields"]), block)
+        for field in block_meta["fields"]:
+            request, response = \
+                    SimServer.subscribe_request_response((block, field))
+            write_lines_to_file(
+                    '%(canned_data)s/Sub/%(block_path)s/'
+                            'request_%(field)s.json' % locals(), request)
+            write_lines_to_file(
+                    '%(canned_data)s/Sub/%(block_path)s/' \
+                            'response_%(field)s.json' % locals(), response)
 
 
 if __name__ == "__main__":
