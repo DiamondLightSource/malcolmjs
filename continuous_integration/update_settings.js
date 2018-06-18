9e0b7f9a9f545d6732af350ace25e1e34f34b9aa
Ray Millward <raymond.millward@tessella.com>
2018-06-18 09:59:01 +0100

    Added the working directory in to the setting script

--- continuous_integration/update_settings.js
+++ continuous_integration/update_settings.js
@@ -4,9 +4,9 @@ var fs = require('fs');
 console.log(process.argv)
 if (process.argv[2] === 'dev') {
   console.log('checking if settings already exist...');
-  if (!fs.existsSync('./public/settings.json')) {
+  if (!fs.existsSync(process.cwd() + '/public/settings.json')) {
       console.log('copying dev settings over');
-      fs.copyFileSync('./settings.dev.json', './public/settings.json');
+      fs.copyFileSync(process.cwd() + '/settings.dev.json', process.cwd() + '/public/settings.json');
   } else {
     console.log('settings exist...doing nothing')
   }
@@ -14,11 +14,8 @@ if (process.argv[2] === 'dev') {
   const gitInfo = execSync('git describe --tags --long').toString();
   console.log('Packaging version ' + gitInfo);
 
-  console.log(process.cwd())
-  console.log(__dirname);
-  let prodConfig = JSON.parse(fs.readFileSync('./settings.prod.json', 'utf8'));
-  console.log(prodConfig)
+  let prodConfig = JSON.parse(fs.readFileSync(process.cwd() + '/settings.prod.json', 'utf8'));
   prodConfig.version = gitInfo.replace('\r', '').replace('\n', '');
 
-  fs.writeFileSync('./build/settings.json', JSON.stringify(prodConfig, null, 2), 'utf8');
+  fs.writeFileSync(process.cwd() + '/build/settings.json', JSON.stringify(prodConfig, null, 2), 'utf8');
 }
