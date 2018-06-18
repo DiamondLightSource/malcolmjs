f2518a9d40536710acb2ea93f9ef64e222c8ff64
Ray Millward <raymond.millward@tessella.com>
2018-06-18 10:31:46 +0100

    Added the build directory if it doesn't exist

--- continuous_integration/update_settings.js
+++ continuous_integration/update_settings.js
@@ -4,9 +4,9 @@ var fs = require('fs');
 console.log(process.argv)
 if (process.argv[2] === 'dev') {
   console.log('checking if settings already exist...');
-  if (!fs.existsSync(process.cwd() + '/public/settings.json')) {
+  if (!fs.existsSync(__dirname + '/../public/settings.json')) {
       console.log('copying dev settings over');
-      fs.copyFileSync(process.cwd() + '/settings.dev.json', process.cwd() + '/public/settings.json');
+      fs.copyFileSync(__dirname + '/../settings.dev.json', process.cwd() + '/public/settings.json');
   } else {
     console.log('settings exist...doing nothing')
   }
@@ -14,8 +14,11 @@ if (process.argv[2] === 'dev') {
   const gitInfo = execSync('git describe --tags --long').toString();
   console.log('Packaging version ' + gitInfo);
 
-  let prodConfig = JSON.parse(fs.readFileSync(process.cwd() + '/settings.prod.json', 'utf8'));
+  let prodConfig = JSON.parse(fs.readFileSync(__dirname + '/../settings.prod.json', 'utf8'));
   prodConfig.version = gitInfo.replace('\r', '').replace('\n', '');
 
-  fs.writeFileSync(process.cwd() + '/build/settings.json', JSON.stringify(prodConfig, null, 2), 'utf8');
+  if (!fs.existsSync(__dirname + '/../build')){
+    fs.mkdirSync(__dirname + '/../build');
+  }
+  fs.writeFileSync(__dirname + '/../build/settings.json', JSON.stringify(prodConfig, null, 2), 'utf8');
 }
