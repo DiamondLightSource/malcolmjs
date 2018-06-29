4ed373462e7a36d4d7327755616e89f80795cf63
Ray Millward <raymond.millward@tessella.com>
2018-06-29 16:27:19 +0100

    Fixed the screenshots so the snackbar isn't present

--- cypress/automated_screenshots/smg_screenshots.js
+++ cypress/automated_screenshots/smg_screenshots.js
@@ -2,7 +2,8 @@ describe('SMG screenshots', () => {
   it('example UI', () => {
     cy.viewport(1200, 675);
     cy.visit('/gui/PANDA/layout/PANDA:CLOCKS');
-    cy.wait(4000);
+    cy.wait(3000);
+    cy.wait(1000);
     cy.screenshot('example-ui');
   });
 
