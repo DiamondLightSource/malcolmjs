d5cca5d5351fec99f7d494564396111a4f38b327
Ray Millward <raymond.millward@tessella.com>
2018-07-30 21:12:38 +0100

    Tried writing the async test differently to see if that helps on travis

--- src/malcolm/actions/autoLayout.action.test.js
+++ src/malcolm/actions/autoLayout.action.test.js
@@ -2,7 +2,7 @@ import autoLayoutAction from './autoLayout.action';
 import { MalcolmSend, MalcolmAttributeFlag } from '../malcolm.types';
 
 describe('auto layout actions', () => {
-  it('runAutoLayout updates block positions', async () => {
+  it('runAutoLayout updates block positions', done => {
     const action = autoLayoutAction.runAutoLayout();
 
     const actions = [];
@@ -69,23 +69,25 @@ describe('auto layout actions', () => {
       },
     });
 
-    await action(dispatch, getState);
+    action(dispatch, getState).then(() => {
+      expect(actions).toHaveLength(2);
+      expect(actions[0].type).toEqual(MalcolmAttributeFlag);
+      expect(actions[0].payload.path).toEqual(['PANDA', 'layout']);
+      expect(actions[0].payload.flagType).toEqual('pending');
+      expect(actions[0].payload.flagState).toEqual(true);
 
-    expect(actions).toHaveLength(2);
-    expect(actions[0].type).toEqual(MalcolmAttributeFlag);
-    expect(actions[0].payload.path).toEqual(['PANDA', 'layout']);
-    expect(actions[0].payload.flagType).toEqual('pending');
-    expect(actions[0].payload.flagState).toEqual(true);
+      expect(actions[1].type).toEqual(MalcolmSend);
+      expect(actions[1].payload.typeid).toEqual('malcolm:core/Put:1.0');
+      expect(actions[1].payload.path).toEqual(['PANDA', 'layout', 'value']);
+      expect(actions[1].payload.value).toEqual({
+        mri: ['block1', 'block2'],
+        name: ['block 1', 'block 2'],
+        visible: [true, true],
+        x: [-107, 107],
+        y: [0, 0],
+      });
 
-    expect(actions[1].type).toEqual(MalcolmSend);
-    expect(actions[1].payload.typeid).toEqual('malcolm:core/Put:1.0');
-    expect(actions[1].payload.path).toEqual(['PANDA', 'layout', 'value']);
-    expect(actions[1].payload.value).toEqual({
-      mri: ['block1', 'block2'],
-      name: ['block 1', 'block 2'],
-      visible: [true, true],
-      x: [-107, 107],
-      y: [0, 0],
+      done();
     });
   });
 });
