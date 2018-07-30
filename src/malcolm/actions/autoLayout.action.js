2e7276d09b460a03c35d4dfb1c45a7f58fdc3ca3
Ray Millward <raymond.millward@tessella.com>
2018-07-30 20:56:33 +0100

    Added a catch statement to see if that is what is failing

--- src/malcolm/actions/autoLayout.action.js
+++ src/malcolm/actions/autoLayout.action.js
@@ -58,33 +58,39 @@ const runAutoLayout = () => (dispatch, getState) => {
   );
 
   const elk = new ELK();
-  return elk.layout(graph).then(graphLayout => {
-    console.log('completed auto layout');
-    const centerX =
-      graphLayout.children.reduce((prev, next) => prev + next.x, 0) /
-      graphLayout.children.length;
-    const centerY =
-      graphLayout.children.reduce((prev, next) => prev + next.y, 0) /
-      graphLayout.children.length;
+  return elk
+    .layout(graph)
+    .then(graphLayout => {
+      console.log('completed auto layout');
+      const centerX =
+        graphLayout.children.reduce((prev, next) => prev + next.x, 0) /
+        graphLayout.children.length;
+      const centerY =
+        graphLayout.children.reduce((prev, next) => prev + next.y, 0) /
+        graphLayout.children.length;
 
-    // we only need to send an update for the visible blocks
-    const updatedLayout = {
-      name: blocks.map(b => b.name),
-      visible: blocks.map(b => b.visible),
-      mri: blocks.map(b => b.mri),
-      x: blocks
-        .map(b => b.mri)
-        .map(mri => graphLayout.children.find(c => c.id === mri).x - centerX),
-      y: blocks
-        .map(b => b.mri)
-        .map(mri => graphLayout.children.find(c => c.id === mri).y - centerY),
-    };
+      // we only need to send an update for the visible blocks
+      const updatedLayout = {
+        name: blocks.map(b => b.name),
+        visible: blocks.map(b => b.visible),
+        mri: blocks.map(b => b.mri),
+        x: blocks
+          .map(b => b.mri)
+          .map(mri => graphLayout.children.find(c => c.id === mri).x - centerX),
+        y: blocks
+          .map(b => b.mri)
+          .map(mri => graphLayout.children.find(c => c.id === mri).y - centerY),
+      };
 
-    dispatch(malcolmSetFlag([parentBlock, mainAttribute], 'pending', true));
-    dispatch(
-      malcolmPutAction([parentBlock, mainAttribute, 'value'], updatedLayout)
-    );
-  });
+      dispatch(malcolmSetFlag([parentBlock, mainAttribute], 'pending', true));
+      dispatch(
+        malcolmPutAction([parentBlock, mainAttribute, 'value'], updatedLayout)
+      );
+    })
+    .catch(err => {
+      console.log('error creating auto layout');
+      console.log(err);
+    });
 };
 
 export default {
