b06cd0982745fd13beb2ad463bf4d6d2f03221b2
Bryan Robert Tester <bryan.tester@diamond.ac.uk>
2018-10-31 17:43:41 +0000

    Fixed & improved unit tests

--- src/malcolm/malcolmHandlers/blockMetaHandler.test.js
+++ src/malcolm/malcolmHandlers/blockMetaHandler.test.js
@@ -21,7 +21,7 @@ describe('block meta handler', () => {
       fields: ['health', 'icon'],
     };
 
-    BlockMetaHandler(request, changes, dispatch);
+    BlockMetaHandler(request, changes, dispatch, {});
 
     expect(dispatches.length).toEqual(3);
     expect(dispatches[0].type).toEqual(MalcolmBlockMeta);
@@ -32,9 +32,81 @@ describe('block meta handler', () => {
     expect(dispatches[0].payload.fields).toEqual(['health', 'icon']);
 
     expect(dispatches[1].type).toEqual(MalcolmSend);
+    expect(dispatches[1].payload.typeid).toEqual('malcolm:core/Subscribe:1.0');
     expect(dispatches[1].payload.path).toEqual(['block1', 'health']);
 
     expect(dispatches[2].type).toEqual(MalcolmSend);
+    expect(dispatches[2].payload.typeid).toEqual('malcolm:core/Subscribe:1.0');
     expect(dispatches[2].payload.path).toEqual(['block1', 'icon']);
+  });
+
+  it('doesnt send duplicate subscription', () => {
+    const changes = {
+      typeid: 'DELTA',
+      label: 'Block 1',
+      fields: ['health', 'icon'],
+    };
+
+    BlockMetaHandler(request, changes, dispatch, {
+      test: {
+        typeid: 'malcolm:core/Subscribe:1.0',
+        path: ['block1', 'health'],
+      },
+    });
+
+    expect(dispatches.length).toEqual(2);
+    expect(dispatches[0].type).toEqual(MalcolmBlockMeta);
+    expect(dispatches[0].payload.id).toEqual(1);
+    expect(dispatches[0].payload.typeid).toEqual('DELTA');
+    expect(dispatches[0].payload.delta).toEqual(true);
+    expect(dispatches[0].payload.label).toEqual('Block 1');
+    expect(dispatches[0].payload.fields).toEqual(['health', 'icon']);
+    expect(dispatches[1].type).toEqual(MalcolmSend);
+    expect(dispatches[1].payload.path).toEqual(['block1', 'icon']);
+  });
+
+  it('doesnt send subscription if flag set to false', () => {
+    const changes = {
+      typeid: 'DELTA',
+      label: 'Block 1',
+      fields: ['health', 'icon'],
+    };
+
+    BlockMetaHandler(request, changes, dispatch, {}, false);
+
+    expect(dispatches.length).toEqual(1);
+    expect(dispatches[0].type).toEqual(MalcolmBlockMeta);
+    expect(dispatches[0].payload.id).toEqual(1);
+    expect(dispatches[0].payload.typeid).toEqual('DELTA');
+    expect(dispatches[0].payload.delta).toEqual(true);
+    expect(dispatches[0].payload.label).toEqual('Block 1');
+  });
+
+  it('sends gets if flag true', () => {
+    const changes = {
+      typeid: 'DELTA',
+      label: 'Block 1',
+      fields: ['health', 'icon'],
+    };
+
+    BlockMetaHandler(request, changes, dispatch, {}, false, true);
+
+    expect(dispatches.length).toEqual(3);
+    expect(dispatches[0].type).toEqual(MalcolmBlockMeta);
+    expect(dispatches[0].payload.id).toEqual(1);
+    expect(dispatches[0].payload.typeid).toEqual('DELTA');
+    expect(dispatches[0].payload.delta).toEqual(true);
+    expect(dispatches[0].payload.label).toEqual('Block 1');
+    expect(dispatches[0].payload.fields).toEqual(['health', 'icon']);
+
+    dispatches[1](dispatch, () => ({ malcolm: { blocks: { block1: {} } } }));
+    dispatches[2](dispatch, () => ({ malcolm: { blocks: { block1: {} } } }));
+    expect(dispatches[3].type).toEqual(MalcolmSend);
+    expect(dispatches[3].payload.typeid).toEqual('malcolm:core/Get:1.0');
+    expect(dispatches[3].payload.path).toEqual(['block1', 'health']);
+
+    expect(dispatches[4].type).toEqual(MalcolmSend);
+    expect(dispatches[4].payload.typeid).toEqual('malcolm:core/Get:1.0');
+    expect(dispatches[4].payload.path).toEqual(['block1', 'icon']);
   });
 });
