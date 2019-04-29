8c0db3521a6cfa97d94900e6202a53efa4803a15
Bryan Robert Tester <bryan.tester@diamond.ac.uk>
2019-04-29 14:27:18 +0100

    Added redirect handling to AppRouter

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import MainMalcolmView from './mainMalcolmView/mainMalcolmView.container';
 import malcolmPopOut from './mainMalcolmView/malcolmPopOut.container';
 
@@ -7,6 +7,8 @@ const AppRouter = () => (
   <div>
     <Route exact path="/gui/*" component={MainMalcolmView} />
     <Route exact path="/details/*" component={malcolmPopOut} />
+    <Route exact path="/" render={() => <Redirect to="/gui/" />} />
+    <Route exact path="/gui" render={() => <Redirect to="/gui/" />} />
   </div>
 );
 
